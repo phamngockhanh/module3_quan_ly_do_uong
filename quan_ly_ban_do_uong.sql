b4ccbd014b825947d7a5ba53f8a19925a0664b5f
Vinhnd03 <ducvinhmycanh@gmail.com>
2025-05-26 15:16:02 +0700

    tao database

---
+++ quan_ly_ban_do_uong.sql
@@ -0,0 +1,296 @@
+CREATE DATABASE  IF NOT EXISTS `quan_ly_ban_do_uong` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `quan_ly_ban_do_uong`;
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: quan_ly_ban_do_uong
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `gio_hang`
+--
+
+DROP TABLE IF EXISTS `gio_hang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gio_hang` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ngay_dat` datetime DEFAULT NULL,
+  `trang_thai` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gio_hang`
+--
+
+LOCK TABLES `gio_hang` WRITE;
+/*!40000 ALTER TABLE `gio_hang` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gio_hang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gio_hang_chi_tiet`
+--
+
+DROP TABLE IF EXISTS `gio_hang_chi_tiet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gio_hang_chi_tiet` (
+  `id_gio_hang` int NOT NULL,
+  `id_san_pham` int NOT NULL,
+  `so_luong` int DEFAULT NULL,
+  PRIMARY KEY (`id_gio_hang`,`id_san_pham`),
+  KEY `id_san_pham` (`id_san_pham`),
+  CONSTRAINT `gio_hang_chi_tiet_ibfk_1` FOREIGN KEY (`id_gio_hang`) REFERENCES `gio_hang` (`id`),
+  CONSTRAINT `gio_hang_chi_tiet_ibfk_2` FOREIGN KEY (`id_san_pham`) REFERENCES `san_pham` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gio_hang_chi_tiet`
+--
+
+LOCK TABLES `gio_hang_chi_tiet` WRITE;
+/*!40000 ALTER TABLE `gio_hang_chi_tiet` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gio_hang_chi_tiet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hoa_don`
+--
+
+DROP TABLE IF EXISTS `hoa_don`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hoa_don` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_nguoi_dung` int DEFAULT NULL,
+  `ngay_dat` datetime DEFAULT NULL,
+  `trang_thai` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_nguoi_dung` (`id_nguoi_dung`),
+  CONSTRAINT `hoa_don_ibfk_1` FOREIGN KEY (`id_nguoi_dung`) REFERENCES `nguoi_dung` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hoa_don`
+--
+
+LOCK TABLES `hoa_don` WRITE;
+/*!40000 ALTER TABLE `hoa_don` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hoa_don` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hoa_don_chi_tiet`
+--
+
+DROP TABLE IF EXISTS `hoa_don_chi_tiet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hoa_don_chi_tiet` (
+  `id_hoa_don` int NOT NULL,
+  `id_san_pham` int NOT NULL,
+  `so_luong` int DEFAULT NULL,
+  PRIMARY KEY (`id_hoa_don`,`id_san_pham`),
+  KEY `id_san_pham` (`id_san_pham`),
+  CONSTRAINT `hoa_don_chi_tiet_ibfk_1` FOREIGN KEY (`id_hoa_don`) REFERENCES `hoa_don` (`id`),
+  CONSTRAINT `hoa_don_chi_tiet_ibfk_2` FOREIGN KEY (`id_san_pham`) REFERENCES `san_pham` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hoa_don_chi_tiet`
+--
+
+LOCK TABLES `hoa_don_chi_tiet` WRITE;
+/*!40000 ALTER TABLE `hoa_don_chi_tiet` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hoa_don_chi_tiet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loai_san_pham`
+--
+
+DROP TABLE IF EXISTS `loai_san_pham`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `loai_san_pham` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ten_loai` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loai_san_pham`
+--
+
+LOCK TABLES `loai_san_pham` WRITE;
+/*!40000 ALTER TABLE `loai_san_pham` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loai_san_pham` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nguoi_dung`
+--
+
+DROP TABLE IF EXISTS `nguoi_dung`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nguoi_dung` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ten_nguoi_dung` varchar(50) DEFAULT NULL,
+  `id_tai_khoan` int DEFAULT NULL,
+  `so_dien_thoai` varchar(11) DEFAULT NULL,
+  `dia_chi` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `id_gio_hang` int DEFAULT NULL,
+  `trang_thai` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_tai_khoan` (`id_tai_khoan`),
+  UNIQUE KEY `id_gio_hang` (`id_gio_hang`),
+  CONSTRAINT `nguoi_dung_ibfk_1` FOREIGN KEY (`id_tai_khoan`) REFERENCES `tai_khoan` (`id`),
+  CONSTRAINT `nguoi_dung_ibfk_2` FOREIGN KEY (`id_gio_hang`) REFERENCES `gio_hang` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nguoi_dung`
+--
+
+LOCK TABLES `nguoi_dung` WRITE;
+/*!40000 ALTER TABLE `nguoi_dung` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nguoi_dung` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `san_pham`
+--
+
+DROP TABLE IF EXISTS `san_pham`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `san_pham` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ten_san_pham` varchar(50) DEFAULT NULL,
+  `gia` bigint DEFAULT NULL,
+  `id_loai_san_pham` int DEFAULT NULL,
+  `trang_thai` tinyint(1) DEFAULT NULL,
+  `mo_ta` text,
+  `hinh_anh` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_loai_san_pham` (`id_loai_san_pham`),
+  CONSTRAINT `san_pham_ibfk_1` FOREIGN KEY (`id_loai_san_pham`) REFERENCES `san_pham` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `san_pham`
+--
+
+LOCK TABLES `san_pham` WRITE;
+/*!40000 ALTER TABLE `san_pham` DISABLE KEYS */;
+/*!40000 ALTER TABLE `san_pham` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tai_khoan`
+--
+
+DROP TABLE IF EXISTS `tai_khoan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tai_khoan` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ten_dang_nhap` varchar(30) DEFAULT NULL,
+  `mat_khau` varchar(50) DEFAULT NULL,
+  `id_vai_tro` int DEFAULT NULL,
+  `token` varchar(50) DEFAULT NULL,
+  `trang_thai` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ten_dang_nhap` (`ten_dang_nhap`),
+  KEY `id_vai_tro` (`id_vai_tro`),
+  CONSTRAINT `tai_khoan_ibfk_1` FOREIGN KEY (`id_vai_tro`) REFERENCES `vai_tro` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tai_khoan`
+--
+
+LOCK TABLES `tai_khoan` WRITE;
+/*!40000 ALTER TABLE `tai_khoan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tai_khoan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trang_thai_hoa_don`
+--
+
+DROP TABLE IF EXISTS `trang_thai_hoa_don`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trang_thai_hoa_don` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ten_trang_thai` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trang_thai_hoa_don`
+--
+
+LOCK TABLES `trang_thai_hoa_don` WRITE;
+/*!40000 ALTER TABLE `trang_thai_hoa_don` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trang_thai_hoa_don` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vai_tro`
+--
+
+DROP TABLE IF EXISTS `vai_tro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vai_tro` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ten_vai_tro` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vai_tro`
+--
+
+LOCK TABLES `vai_tro` WRITE;
+/*!40000 ALTER TABLE `vai_tro` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vai_tro` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-26 15:14:29
