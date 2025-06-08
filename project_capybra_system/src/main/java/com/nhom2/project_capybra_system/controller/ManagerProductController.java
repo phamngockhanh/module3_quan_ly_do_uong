6736e63bab2f86adc3651886bfb79910edac0b54
unknown <le17112401@gmail.com>
2025-06-08 19:28:04 +0700

    add

---
+++ project_capybra_system/src/main/java/com/nhom2/project_capybra_system/controller/ManagerProductController.java
@@ -0,0 +1,17 @@
+package com.nhom2.project_capybra_system.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(value = "/managerProduct")
+public class ManagerProductController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/view/admin/managerProduct.jsp").forward(req, resp);
+    }
+}
